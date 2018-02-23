5c0ab97b81e4db1c23b52a23f7d7bcc55f06eb1a
Ahmed Eid <ahmed.eid@gmx.com>
2018-02-23 18:32:38 +0300

    Added test cases and jar file

--- knapsack-optimizer-service/src/test/java/com/maerskdigital/task/test/KnapsackControllerTests.java
+++ knapsack-optimizer-service/src/test/java/com/maerskdigital/task/test/KnapsackControllerTests.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.maerskdigital.task.test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,18 +23,44 @@ public class KnapsackControllerTests {
     private MockMvc mockMvc;
 
     @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    public void submitTask() throws Exception{
+    	/*
+      	this.mockMvc.perform(post("/knapsack/admin/tasks"))
+                    .andDo(print()).andExpect(status().isOk());
+    	 */  
     }
-
+    
     @Test
-    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    public void queryTaskStatus() throws Exception{
+    	
+      	this.mockMvc.perform(get("/knapsack/tasks/f5083116-2d96-49ca-9020-e9f22fa0c3bb"))
+                    .andDo(print()).andExpect(status().isOk());
+    	
+    }
+    
+    @Test
+    public void getSolution() throws Exception{
+    	
+      	this.mockMvc.perform(get("/knapsack/solutions/f5083116-2d96-49ca-9020-e9f22fa0c3bb"))
+            .andDo(print()).andExpect(status().isOk());
+    	
+    }
+    
+    @Test
+    public void getTasks() throws Exception{
+    	
+     	this.mockMvc.perform(get("/knapsack/admin/tasks"))
+            .andDo(print()).andExpect(status().isOk());
+    	
+    }
+    
+    @Test
+    public void shutdown() throws Exception{
+    	/*
+    	  this.mockMvc.perform(post("/admin/shutdown"))
+          .andDo(print()).andExpect(status().isOk())
+          .andExpect(jsonPath("$.content").value("Service shutting down...\n"));
+    	*/
     }
 
 }
